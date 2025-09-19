329715e6e38170c81d2a8dd6663c05b693fc0733
Talom Laël <gesee37@gmail.com>
2025-09-19 07:36:29 +0100

    Create three_sum.rs

---
+++ Medium/three_sum.rs
@@ -0,0 +1,37 @@
+impl Solution {
+    pub fn three_sum(mut nums: Vec<i32>) -> Vec<Vec<i32>> {
+        let mut res = Vec::new();
+        nums.sort();
+
+        let n = nums.len();
+        for i in 0..n {
+            if i > 0 && nums[i] == nums[i - 1] {
+                continue; // éviter doublons
+            }
+
+            let (mut left, mut right) = (i + 1, n - 1);
+            while left < right {
+                let sum = nums[i] + nums[left] + nums[right];
+                if sum == 0 {
+                    res.push(vec![nums[i], nums[left], nums[right]]);
+                    
+                    // avancer left et right en sautant les doublons
+                    while left < right && nums[left] == nums[left + 1] {
+                        left += 1;
+                    }
+                    while left < right && nums[right] == nums[right - 1] {
+                        right -= 1;
+                    }
+                    left += 1;
+                    right -= 1;
+                } else if sum < 0 {
+                    left += 1;
+                } else {
+                    right -= 1;
+                }
+            }
+        }
+
+        res
+    }
+}
